a2842c4f7d2e33e2edcc5a7d2498d96933802fcd
Sam Morgen <samorgen14@gmail.com>
2024-01-05 16:45:40 -0800

    jwt

--- server/app.ts
+++ server/app.ts
@@ -1,24 +1,46 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
+import jwt from 'jsonwebtoken'
 
-const PORT = 8080;
-const app = express();
-const database = { data: "Hello World" };
+const secret = 'secret'
+const PORT = 8080
+const app = express()
+const database = { data: 'Hello World' }
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+
+const auth = (req: any, res: any, next: any) => {
+  const token = req.body.token
+
+  if (!token) {
+    res.sendStatus(401)
+  } else {
+    jwt.verify(token, secret, function (err: any) {
+      if (err) {
+        res.sendStatus(401)
+      } else {
+        next()
+      }
+    })
+  }
+}
 
 // Routes
 
-app.get("/", (req, res) => {
-  res.json(database);
-});
+app.get('/', (req, res) => {
+  let token = jwt.sign({ claims: database, expiresIn: '1hr' }, secret)
 
-app.post("/", (req, res) => {
-  database.data = req.body.data;
-  res.sendStatus(200);
-});
+  res.json({ data: database, token: token })
+})
+
+app.post('/', auth, (req, res) => {
+  database.data = req.body.data
+
+  res.send({ data: database.data })
+  //res.sendStatus(200)
+})
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+  console.log('Server running on port ' + PORT)
+})
